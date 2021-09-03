70f4e897afce6a26d95e45e76b283b3e99d5739f
dtuckersuh <suh977@gmail.com>
2021-09-02 22:52:13 -0700

    Add addItem and removeItem functionality to Checkout component

--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 export const cartSlice = createSlice({
     name: 'cart',
@@ -13,9 +13,16 @@ export const cartSlice = createSlice({
         },
         addItem: (state, action) => {
             state.cartItems = addItemToCart(state.cartItems, action.payload);
+        },
+        clearItemFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter(cartItem =>
+                cartItem.id !== action.payload.id)
+        },
+        removeItem: (state, action) => {
+            state.cartItems = removeItemFromCart(state.cartItems, action.payload);
         }
     },
 })
 
-export const { toggleCartHidden, addItem } = cartSlice.actions;
+export const { toggleCartHidden, addItem, clearItemFromCart, removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
