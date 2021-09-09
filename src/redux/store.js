c7bfd4f8b7f8f4fd9f9d6688acb39b1bcf4562d1
dtuckersuh <suh977@gmail.com>
2021-09-08 18:56:27 -0700

    Add Redux Persist functionality to store.js, Update imageUrls in directory component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger';
 
 import userReducer from './user/userSlice';
 import cartReducer from './cart/cartSlice';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] // Cart will be persisted
+}
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
     // automatically combines reducers into root reducer
-    reducer: {
-        user: userReducer,
-        cart: cartReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    reducer: persistedReducer, 
+    middleware: [logger],
 });
+
+// Ensures redux state is saved to persisted storage
+export const persistor = persistStore(store);
 
 export default store;
